a3e656feda2990b8b4e1ac4407ad34e07f957914
Christina <56486108+csarmiento92@users.noreply.github.com>
2019-10-27 16:47:06 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace inputs_and_functions
+{
+    class Program
+    {
+
+        public static int Sum(int num1, int num2)
+    {
+        int total;
+        total = num1 + num2;
+        return total;
+    }
+
+        static void Main(string[] args)
+        {
+            // Create an application that asks the user for two different numbers
+            // then writes the sum of the numbers to the console as the answer.
+            // Create variables to hold the values of the first and second numbers
+
+            // Have the console prompt for each input
+            // Store the console response in each of the variables
+
+            // Use a separate function to calculate the sum.
+
+            // Write the result of the function call to the console
+           
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1);
+            Console.Write("Enter another number: ");
+            int num2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(num1,num2) );
+
+        }
+
+    }
+}
